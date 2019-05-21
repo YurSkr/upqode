13b74da86b5b8707482f181b8c25cf36f539ffa3
Yurii Skrypets <yurik.plast@ua.fm>
2019-05-21 17:55:19 +0300

    Update carouselInterval.js

--- js/carouselInterval.js
+++ js/carouselInterval.js
@@ -1,5 +1,5 @@
-const carouselItems = document.getElementsByClassName('carousel-item');
-for (let i = 0; i < carouselItems.length; i++) {
-    const carouselItem = carouselItems[i];
+var carouselItems = document.getElementsByClassName('carousel-item');
+for (var i = 0; i < carouselItems.length; i++) {
+    var carouselItem = carouselItems[i];
     carouselItem.setAttribute('data-interval', String(Math.floor(Math.random() * (16000 - 8000) + 8000)));
 }
