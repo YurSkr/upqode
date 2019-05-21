d2cf3b464d8352bc2a6d9a717d0b25e08e2b5a75
Yurii Skrypets <yurik.plast@ua.fm>
2019-05-21 18:23:44 +0300

    Update activeNav.js

--- js/activeNav.js
+++ js/activeNav.js
@@ -6,22 +6,53 @@ for (var i = 0; i < aElements.length; i++) {
         var a = aElements[i];
         a.addEventListener('click', function(event) {
             var current = document.getElementsByClassName('activeNav')[0];
-            return current.classList.remove('activeNav');
+             current.classList.remove('activeNav');
             var thisA = event.target;
             if (dropdownMain.classList.contains('activeNav')) {
                 dropdownMain.classList.remove('activeNav')
-            } else  return thisA.classList.add('activeNav');
+            } 
+            thisA.classList.add('activeNav');
         });
         (function () {
             for (var j = 0; j < productsA.length; j++) {
                 var dropdownAElement = productsA[j];
                 dropdownAElement.addEventListener('click', function() {
                     if (a.classList.contains('activeNav')) {
-                        return a.classList.remove('activeNav')
-                    } else return dropdownMain.classList.add('activeNav');
+                         a.classList.remove('activeNav')
+                    }
+                    dropdownMain.classList.add('activeNav');
                 });
                 dropdownAElement.setAttribute('href', '#products');
             }
         })();
     })();
 }
+var fixedTop = document.querySelector('.fixed-top');
+var section = document.querySelectorAll('.section');
+window.addEventListener('scroll', function() {
+    function moreThen(number) {
+        return windowFromTop > section[number].offsetTop
+    }
+    function lessThen(number) {
+        return windowFromTop < section[number].offsetTop
+    }
+    function addClass(number){
+        return aElements[number].classList.add('activeNav')
+    }
+    function removeClass(number) {
+        return aElements[number].classList.remove('activeNav');
+    }
+    var windowFromTop = window.scrollY + fixedTop.offsetHeight;
+    if (moreThen(0) && lessThen(1)) {
+         addClass(0)
+    } else removeClass(0);
+    if (moreThen(1) && lessThen(2)) {
+        dropdownMain.classList.add('activeNav');
+    } else dropdownMain.classList.remove('activeNav');
+    if (moreThen(2) && lessThen(3)) {
+        addClass(1)
+    } else removeClass(1);
+    if (moreThen(3)) {
+        addClass(2)
+    } else removeClass(2)
+});
