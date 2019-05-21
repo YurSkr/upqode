a5ffbc760a99335a5e10222a1aca0ae2d38a8d30
Yurii Skrypets <yurik.plast@ua.fm>
2019-05-21 18:02:32 +0300

    Update dropdown.js

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,12 +1,15 @@
 var dropdownItems = document.getElementsByClassName('dropdown__submenu');
 
 for (var i = 0; i < dropdownItems.length; i++) {
-    var dropdownItem = dropdownItems[i];
-    var dropdownItemChild = dropdownItem.firstChild;
-    dropdownItem.addEventListener('mouseenter', function() {
-        dropdownItemChild.firstChild.innerHTML = '&raquo; '
-    });
-    dropdownItem.addEventListener('mouseleave', function() {
-        dropdownItemChild.firstChild.innerHTML = '&rsaquo; '
-    })
+    (function () {
+        var dropdownItem = dropdownItems[i];
+        var dropdownItemChild = dropdownItem.firstChild;
+        dropdownItem.addEventListener('mouseenter', function() {
+            return dropdownItemChild.firstChild.innerHTML = '&raquo; '
+        });
+        dropdownItem.addEventListener('mouseleave', function() {
+            return dropdownItemChild.firstChild.innerHTML = '&rsaquo; '
+        })
+    })()
+    
 }
