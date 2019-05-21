78edbaa7117cdcde08229dbf73cf772fd0a03564
Yurii Skrypets <yurik.plast@ua.fm>
2019-05-21 18:04:33 +0300

    Update activeNav.js

--- js/activeNav.js
+++ js/activeNav.js
@@ -2,26 +2,30 @@ var aElements = document.querySelectorAll('.main-nav__a');
 var dropdownMain = document.querySelector('.dropdown__main');
 var productsA = document.querySelectorAll('.dropdown-levels a');
 for (var i = 0; i < aElements.length; i++) {
-    var a = aElements[i];
-    a.addEventListener('click', function(event) {
-        var current = document.getElementsByClassName('activeNav')[0];
-        current.classList.remove('activeNav');
-        var thisA = event.target;
-        thisA.classList.add('activeNav');
-        if (dropdownMain.classList.contains('activeNav')) {
-            dropdownMain.classList.remove('activeNav')
-        }
-    });
-    for (var j = 0; j < productsA.length; j++) {
-        var dropdownAElement = productsA[j];
-        dropdownAElement.addEventListener('click', function() {
-            dropdownMain.classList.add('activeNav');
-            if (a.classList.contains('activeNav')) {
-                a.classList.remove('activeNav')
+    (function () {
+        var a = aElements[i];
+        a.addEventListener('click', function(event) {
+            var current = document.getElementsByClassName('activeNav')[0];
+            current.classList.remove('activeNav');
+            var thisA = event.target;
+            thisA.classList.add('activeNav');
+            if (dropdownMain.classList.contains('activeNav')) {
+                dropdownMain.classList.remove('activeNav')
             }
         });
-        dropdownAElement.setAttribute('href', '#products');
-    }
+        (function () {
+            for (var j = 0; j < productsA.length; j++) {
+                var dropdownAElement = productsA[j];
+                dropdownAElement.addEventListener('click', function() {
+                    dropdownMain.classList.add('activeNav');
+                    if (a.classList.contains('activeNav')) {
+                        a.classList.remove('activeNav')
+                    }
+                });
+                dropdownAElement.setAttribute('href', '#products');
+            }
+        })();
+    })();
 }
 var fixedTop = document.querySelector('.fixed-top');
 var section = document.querySelectorAll('.section');
@@ -52,4 +56,3 @@ window.addEventListener('scroll', function() {
         addClass(2)
     } else removeClass(2)
 });
-
