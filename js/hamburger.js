fe496e1c114ea8c1a5056c14f38e52caae9589ae
Yurii Skrypets <yurik.plast@ua.fm>
2019-05-21 17:49:00 +0300

    Update hamburger.js

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,7 +1,7 @@
 var hamburger = document.getElementById('hamburger');
 var nav = document.querySelector('.header__nav');
 
-hamburger.addEventListener('click', () => {
+hamburger.addEventListener('click', function() {
     hamburger.classList.toggle('open');
     nav.classList.toggle('nav__open')
 });
